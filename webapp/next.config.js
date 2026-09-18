dd8af9b136ec651182f92d24dd724044faf2ab49
frklstn <letoycord@gmail.com>
2026-09-18 19:53:13 +0700

    fix(proxy): add api/v1 reverse proxy rewrites to fin-backend in next.config.js

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://127.0.0.1:3006/api/v1/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
